d187dbe880ce82b6ed3797277d0a242cc1ef70a5
Robert <robertn702@gmail.com>
2014-09-29 10:46:56 +0800

    fixed _.invoke()

--- src/underbar.js
+++ src/underbar.js
@@ -159,6 +159,9 @@ var _ = {};
   // Calls the method named by functionOrKey on each value in the list.
   // Note: you will nead to learn a bit about .apply to complete this.
   _.invoke = function(collection, functionOrKey, args) {
+    return _.map(collection, function(value){
+      return typeof functionOrKey === 'string' ? value[functionOrKey].apply(value, args) : functionOrKey.apply(value, args);
+    });
   };
 
   // Reduces an array or object to a single value by repetitively calling
